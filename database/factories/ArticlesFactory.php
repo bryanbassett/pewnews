77bd49f18bc383fb28d6287ffa52f5bde3351aa1
Bryan James Bassett <bbassett@lilb.CARROTROUTER>
2020-10-08 10:53:49 -0400

    factory seeder ready

---
+++ database/factories/ArticlesFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Articles;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticlesFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Articles::class;
+    public function getImage($url){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+        return $url; 
+    }
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence(6,true),
+            'content' => '<p class="text-2xl">'.$this->faker->realText(100, 2).'</p><p>'.$this->faker->paragraph(3,true).'</p><p>'.$this->faker->paragraph(3,true).'</p>',
+            'imageUrl' => $this->getImage('https://source.unsplash.com/1600x900/'),
+            'user_id' => $this->faker->numberBetween(1,5),
+        ];
+    }
+}
