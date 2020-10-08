136ed4dd7b53bf189c7e18d047ec40a1629122cf
Bryan James Bassett <bbassett@lilb.CARROTROUTER>
2020-10-08 10:55:31 -0400

    one last change

--- database/factories/ArticlesFactory.php
+++ database/factories/ArticlesFactory.php
@@ -13,7 +13,8 @@ class ArticlesFactory extends Factory
      * @var string
      */
     protected $model = Articles::class;
-    public function getImage($url){
+    public function getImage(){
+        $url='https://source.unsplash.com/1600x900/';
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HEADER, true);
@@ -32,7 +33,7 @@ class ArticlesFactory extends Factory
         return [
             'name' => $this->faker->sentence(6,true),
             'content' => '<p class="text-2xl">'.$this->faker->realText(100, 2).'</p><p>'.$this->faker->paragraph(3,true).'</p><p>'.$this->faker->paragraph(3,true).'</p>',
-            'imageUrl' => $this->getImage('https://source.unsplash.com/1600x900/'),
+            'imageUrl' => $this->getImage(),
             'user_id' => $this->faker->numberBetween(1,5),
         ];
     }
